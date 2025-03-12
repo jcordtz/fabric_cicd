d1582fb5e972646c441fd5fbf51a5ab0390469b7
Jan Cordtz <jcordtz@microsoft.com>
2025-03-12 10:30:33 +0100

    Committing 1 item from workspace 6ade1ba3-30f2-452c-89dd-02af902c617a

---
+++ Get_data_demo.Notebook/notebook-content.sql
@@ -0,0 +1,35 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "sqldatawarehouse"
+-- META   },
+-- META   "dependencies": {
+-- META     "warehouse": {
+-- META       "default_warehouse": "c63dcea2-7e56-41c3-ae73-a498a627260f",
+-- META       "known_warehouses": [
+-- META         {
+-- META           "id": "c63dcea2-7e56-41c3-ae73-a498a627260f",
+-- META           "type": "Lakewarehouse"
+-- META         }
+-- META       ]
+-- META     }
+-- META   }
+-- META }
+
+-- CELL ********************
+
+-- Welcome to your new notebook
+-- Type here in the cell editor to add code!
+
+SELECT * FROM deptemp_transform
+
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sql",
+-- META   "language_group": "sqldatawarehouse"
+-- META }
